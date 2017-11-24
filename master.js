c34922a69884adaaf72689a0147641e7087b3b80
Niclas Lindgren <niclas.lindgren@aalto.fi>
2017-11-24 15:56:20 +0200

    Create meal tab ready.

--- master.js
+++ master.js
@@ -21,3 +21,26 @@ function statsTabClicked(e) {
   e.setAttribute('selected', '');
   document.getElementById('tab-content-stats').setAttribute('selected', '');
 }
+
+function addIngredientClicked(e) {
+  let newIngredientElement = document.createElement('div');
+  newIngredientElement.className += ' ingredient';
+  let inputIngredientName = document.createElement('input');
+  inputIngredientName.className = 'ingredientName';
+  inputIngredientName.placeholder = "Ingredient name";
+  let inputIngredientAmount = document.createElement('input');
+  inputIngredientAmount.className = 'ingredientAmount';
+  inputIngredientAmount.placeholder = "Amount";
+  newIngredientElement.appendChild(inputIngredientName);
+  newIngredientElement.appendChild(inputIngredientAmount);
+  let createMealTab = document.getElementById('ingredients');
+  createMealTab.insertBefore(newIngredientElement, e);
+}
+
+function addTemplateClicked(e) {
+
+}
+
+function eatenClicked(e) {
+
+}
